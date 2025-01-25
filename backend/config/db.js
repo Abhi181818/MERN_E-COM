845b313c68ca540c798cb2e5b8b5f04ea7dad10f
Abhishek Yadav <41482390+Abhi181818@users.noreply.github.com>
2025-01-25 10:59:06 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const mongoURI = 'mongodb+srv://abhishek:1234@cluster0.afotl22.mongodb.net/'
+const mongoURI = 'mongodb+srv://abhishek:1234@cluster0.afotl22.mongodb.net/ecom'
 async function connectDb() {
     await mongoose.connect(mongoURI).then(() => console.log("Connected to Mongo Successfully")).catch(err => console.log(err));
 }
