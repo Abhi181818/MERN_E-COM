aaa6ca89c72b7709c617f4e2b4056829f5b98f6e
Abhishek Yadav <41482390+Abhi181818@users.noreply.github.com>
2025-01-25 10:52:37 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 const app=require('./app.js')
 const dotenv=require('dotenv/config')
 const connectDb=require('./config/db.js')
+const cors=require('cors')
 
-
+app.use(cors())
 connectDb()
 app.listen(4000,()=>{
     console.log(`Server is running on port 4000`);
