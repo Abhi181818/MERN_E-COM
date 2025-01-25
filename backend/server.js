65f377b4ed41b04aedfd8f1a5f3fb4605b5561be
Abhishek Yadav <41482390+Abhi181818@users.noreply.github.com>
2025-01-25 10:33:02 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,6 @@ const connectDb=require('./config/db.js')
 
 
 connectDb()
-app.listen(process.env.PORT,()=>{
-    console.log(`Server is running on port ${process.env.PORT} `);
+app.listen(4000,()=>{
+    console.log(`Server is running on port 4000`);
 })
